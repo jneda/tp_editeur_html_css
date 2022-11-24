245871eee6736b87ef4db5c8b566da9eacf8e7c6
jneda <julien.dane@gmail.com>
2022-11-24 16:35:48 +0100

    Commencé implémenter jauge

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let a = document.querySelector('#a');
 let b = document.querySelector('#b');
+let meter = document.querySelector('#meter');
+
+let c = a.value.length;
 
 a.addEventListener('input', function () {
   b.innerHTML = a.value;
+
   let toLimit = 200 - a.value.length;
   console.log(toLimit);
 });
