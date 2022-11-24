71b5088ba9547aad189f8687404a874966cb6b88
jneda <julien.dane@gmail.com>
2022-11-24 18:32:34 +0100

    Ajouté compteurs

--- editor.js
+++ editor.js
@@ -1,4 +1,52 @@
 const editor = document.querySelector(".editor");
 const preview = document.querySelector(".preview");
 
-editor.addEventListener("input", () => (preview.innerHTML = editor.value));
+const maxCharsInput = document.querySelector(".max-chars-input");
+
+const charsMeter = document.querySelector(".chars-meter");
+const countParagraph = document.querySelector(".chars-count");
+
+let charsCount = 0;
+let maxChars = 200;
+
+function init() {
+  editor.value = "";
+  preview.value = "";
+  onEditorUpdate();
+}
+
+function updatePreview() {  
+  // update preview
+  preview.innerHTML = editor.value;
+}
+
+function getTextCount() {
+  console.log(editor.value.replace(/<.*?>/g, '').length);
+}
+
+function updateCountUI() {
+  getTextCount();
+  // update char count
+  charsCount = editor.value.length;
+  // update count paragraph
+  countParagraph.innerText = `${charsCount}/${maxChars}`;
+  // update meter
+  charsMeter.value = charsCount;
+}
+
+function onMaxCharsUpdate() {
+  maxChars = maxCharsInput.value;
+  charsMeter.max = maxChars;
+  updateCountUI();
+}
+
+function onEditorUpdate() {
+  // update UI
+  updateCountUI();
+  updatePreview();
+}
+
+editor.addEventListener("input", onEditorUpdate);
+maxCharsInput.addEventListener("input", onMaxCharsUpdate);
+
+window.addEventListener("load", updateCountUI);
