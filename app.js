5937c771863dcbc241565d5853c11a4cd0de9a04
Mickael Gonzales <mickael.gonzales@hotmail.com>
2022-11-24 17:06:11 +0100

    ajout texte compteur

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ let editor = document.querySelector('#editor');
 let preview = document.querySelector('#preview');
 let meter = document.querySelector('#meter');
 let maxChars = document.querySelector('#maxChars');
+let compteur = document.querySelector('.compteur');
 
 let nbChars;
 
 maxChars.addEventListener('input', function () {
   meter.max = maxChars.value;
+  // compteur.innerText = `${editor.value.length} / ${meter.max}`;
 });
 
 editor.addEventListener('input', function () {
   preview.innerHTML = editor.value;
   nbChars = editor.value.length;
   meter.value = nbChars;
+  compteur.innerText = `${nbChars} / ${meter.max}`;
 
   let toLimit = meter.max - nbChars;
   console.log(`${toLimit}/${meter.max}`);
