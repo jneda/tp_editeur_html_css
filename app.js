9f7c7b9564e9c1f872ceaab805b71babd184262e
jneda <julien.dane@gmail.com>
2022-11-24 16:56:44 +0100

    On peut maintenant modifier la limite de caractères

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 let editor = document.querySelector('#editor');
 let preview = document.querySelector('#preview');
 let meter = document.querySelector('#meter');
+let maxChars = document.querySelector('#maxChars');
+
 let nbChars;
+
+maxChars.addEventListener('input', function () {
+  meter.max = maxChars.value;
+});
 
 editor.addEventListener('input', function () {
   preview.innerHTML = editor.value;
   nbChars = editor.value.length;
   meter.value = nbChars;
 
-  let toLimit = 200 - nbChars;
-  console.log(toLimit);
+  let toLimit = meter.max - nbChars;
+  console.log(`${toLimit}/${meter.max}`);
 });
